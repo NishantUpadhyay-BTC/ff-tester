23ab02a945e900af64d56b2c9e08186b8b1cafc8
NishantUpadhyay-BTC <nishantupadhyay@botreetechnologies.com>
2020-01-15 21:57:30 +0530

    added dashboard controller

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,17 @@
+class DashboardController < ApplicationController
+  def end_point
+    res= params['data']
+    puts res
+    respond_to do |format|
+      format.json do
+        render json: {
+        	"code": "00000",
+        	"message": "Message received",
+        	"docContent": "",
+        	"messageType": "UBI_ORDER",
+        	"serverWFID": "136875"
+        }, status: :ok
+      end
+    end
+  end
+end
